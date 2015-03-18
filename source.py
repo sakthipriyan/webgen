f8efff231c0923c67c53cc9566807ad9ad7cd13b
Sakthi Priyan H <sakthipriyan@crayondata.com>
2015-03-18 20:26:33 +0530

    Files moved back

---
+++ source.py
@@ -0,0 +1,30 @@
+import logging
+import os
+import logging
+import shutil
+
+logger = logging.getLogger(__name__)
+
+def list_blogs(src):
+	blogs = []
+	for year in os.listdir(src):
+		year_dir = src + '/' + year
+		for month in os.listdir(year_dir):
+			month_dir = year_dir + '/' + month
+			for day in os.listdir(month_dir):
+				day_dir = month_dir + '/' + day
+				for article in os.listdir(day_dir):
+					article_path = day_dir + '/' + article
+					blogs.append(article_path)
+	logger.debug('Total number of blogs available %s', len(blogs))
+	return blogs
+
+def copy_files(src,web_copy):
+	src = config['base_dir']
+	web_copy = config['web_copy']
+	logger.info('Source directory %s', src)
+	dist = src + '/dist'
+	shutil.rmtree(dist,True)
+	logger.info('Website is generated at %s', dist)
+	for folder in web_copy:
+		shutil.copytree('%s/web/%s' % (src,folder), '%s/%s' % (dist,folder))
---
+++ webgen.py
@@ -0,0 +1,20 @@
+import logging
+import source
+import sys
+import json
+
+def main():
+	logging.basicConfig(level=logging.DEBUG,format='%(asctime)-15s %(levelname)s %(name)s %(message)s')
+	logger = logging.getLogger(__name__)
+	logger.info('Generating website...')
+	config = json.load(open(sys.argv[1]))
+	src = config['base_dir']
+	web_copy = config['web_copy']
+	source.copy_files(src,web_copy)
+	source.list_blogs(src + '/src')
+	logger.info('Completed')
+
+if __name__ == '__main__':
+	if(len(sys.argv)==1):
+		sys.exit('Usage: %s /path/to/config.json\nMore details at https://github.com/sakthipriyan/webgen' % sys.argv[0])
+	main()
